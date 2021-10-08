3f9c2905ccb01feb1e53a81b5a62700d190e3d26
Incha <Incha@192.168.115.166>
2021-10-08 13:56:18 +0200

    Dépot de TP

--- TP1_manipNombresint_INCHAUD_PEAUCELLE/src/tp1_manipnombresint_inchaud_peaucelle/TP1_manipNombresint_INCHAUD_PEAUCELLE.java
+++ TP1_manipNombresint_INCHAUD_PEAUCELLE/src/tp1_manipnombresint_inchaud_peaucelle/TP1_manipNombresint_INCHAUD_PEAUCELLE.java
@@ -4,6 +4,8 @@
  */
 package tp1_manipnombresint_inchaud_peaucelle;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Incha
@@ -15,6 +17,15 @@ public class TP1_manipNombresint_INCHAUD_PEAUCELLE {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int entier1;
+        int entier2;
+        entier1=sc.nextInt();
+        entier2=sc.nextInt();
+        System.out.println(entier1);
+        System.out.println(entier2);
+        System.out.println("Somme :"+(entier1+entier2)+" différence :"+(entier1-entier2)+" produit :"+(entier1*entier2));
+        System.out.println("Quotient entier :"+(entier1/entier2)+" reste :"+(entier1%entier2));
     }
     
 }
