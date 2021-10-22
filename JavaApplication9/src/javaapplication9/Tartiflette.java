8a5c0bdc5b24f8aad17e9424a717793371cde7cf
cleme <cleme@192.168.115.145>
2021-10-22 12:00:16 +0200

    exercice 3 TP2 2e essai

---
+++ JavaApplication9/src/javaapplication9/Tartiflette.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication9;
+
+/**
+ *
+ * @author cleme
+ */
+public class Tartiflette {
+    int nbCalories;
+    Tartiflette(int x) {
+        nbCalories = x;
+    }
+}
