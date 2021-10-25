cb63919e3f3c2be347bc1f9a8649ae9765d7c6c5
cleme <cleme@192.168.115.145>
2021-10-25 15:17:25 +0200

    Sp4 1ere version

---
+++ SP4_console_INCHAUD_PEAUCELLE/src/sp4_console_inchaud_peaucelle/Cellule.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_inchaud_peaucelle;
+
+/**
+ *
+ * @author cleme
+ */
+public class Cellule {
+    Jeton jetonCourant;
+    boolean trouNoir;
+    boolean desintegrateur;
+}
