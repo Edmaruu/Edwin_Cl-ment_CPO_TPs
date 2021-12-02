a5269b1f7b60b59fe57a5f1a9170107c5fdcd293
Incha <Incha@192.168.115.166>
2021-12-02 19:55:20 +0100

    Création de nos trois classes affichage de la console traitement des cas de la colonne la plus à gauche et la plus à droite pour éviter tout problème d'indentation

--- Light_out_Inchaud_Peaucelle/src/light_out_inchaud_peaucelle/Light_out_Inchaud_Peaucelle.java
+++ Light_out_Inchaud_Peaucelle/src/light_out_inchaud_peaucelle/Light_out_Inchaud_Peaucelle.java
@@ -14,7 +14,9 @@ public class Light_out_Inchaud_Peaucelle {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Partie game= new Partie();
+        game.debuterpartie();
+        
     }
     
 }
---
+++ Light_out_Inchaud_Peaucelle/src/light_out_inchaud_peaucelle/Partie.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package light_out_inchaud_peaucelle;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Incha
+ */
+public class Partie {
+
+    grille grillejeu;
+    Scanner sc = new Scanner(System.in);
+    Random generateurAleat = new Random();
+
+    public void debuterpartie() {
+        grillejeu = new grille();
+        grillejeu.affichergrillesurconsole();
+        jouer();
+        grillejeu.affichergrillesurconsole();
+    }
+
+    public void jouer() {
+        int abscisse;
+        int ordonnee;
+        System.out.println("entrez l'abcisse du point sur lequel vous voulez cliquer");
+        abscisse = sc.nextInt();
+        System.out.println("entrez l'ordonnée du point sur lequel vous voulez cliquer");
+        ordonnee = sc.nextInt();
+        grillejeu.changementetat(abscisse, ordonnee);
+        if (ordonnee == 0) {
+            grillejeu.changementetat(abscisse, ordonnee + 1);
+            if (abscisse == 0) {
+                grillejeu.changementetat(abscisse + 1, ordonnee);
+            } else if (abscisse == 4) {
+                grillejeu.changementetat(abscisse - 1, ordonnee);
+            } else {
+                grillejeu.changementetat(abscisse - 1, ordonnee);
+                grillejeu.changementetat(abscisse + 1, ordonnee);
+            }
+        } else if (ordonnee == 4) {
+            grillejeu.changementetat(abscisse, ordonnee - 1);
+            if (abscisse == 0) {
+                grillejeu.changementetat(abscisse + 1, ordonnee);
+            } else if (abscisse == 4) {
+                grillejeu.changementetat(abscisse - 1, ordonnee);
+            } else {
+                grillejeu.changementetat(abscisse - 1, ordonnee);
+                grillejeu.changementetat(abscisse + 1, ordonnee);
+            }
+        }
+    }
+}
