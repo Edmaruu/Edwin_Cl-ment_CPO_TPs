61741215c2a0993450480db7ae5712ffed16d3fe
Incha <Incha@192.168.115.166>
2022-01-01 21:34:05 +0100

    affichage de la grille 5*5 + case graphique création de bouton clickable donc sur le panneau de jeu

---
+++ Light_out_Inchaud_Peaucelle/src/light_out_inchaud_peaucelle/casegraphique.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package light_out_inchaud_peaucelle;
+import javax.swing.ImageIcon;
+import java.awt.Graphics;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Incha
+ */
+public class casegraphique extends JButton {
+    Case casegraph;
+    
+    public casegraphique(Case unecase){
+        casegraph=unecase;
+    }
+}
